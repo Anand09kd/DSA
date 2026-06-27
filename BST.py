63306974c6457cab92c5cb4fba659d2d3cad2cad
Anand <dishantkumaranand@gmail.com>
2026-06-27 12:42:45 +0530

    BST

---
+++ BST.py
@@ -0,0 +1,84 @@
+from turtle import right
+
+
+class Node:
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.data = value
+
+# Insert function
+def insert(root, value):
+    if root is None:
+        return Node(value)
+    if root.data == value:
+        return root
+    if root.data > value:
+        root.left = insert(root.left, value)
+    else:
+        root.right = insert(root.right, value)
+    return root
+
+# Search function
+def search(root, value):
+    if root is None:
+        print("Element not found")
+        return
+    if root.data == value:
+        print("Element found")
+        return
+    if root.data > value:
+        search(root.left, value)
+    else:
+        search(root.right, value)
+
+# get successor function
+def get_successor(root):
+    root = root.right
+    while(root != None and root.left != None):
+        root = root.left
+    return root
+
+# Deletion function
+def delete(root, value):
+    if(root == None):
+        return root
+    if(root.data > value):
+        root.left = delete(root.left, value)
+    elif(root.data < value):
+        root.right = delete(root.right, value)
+
+    else:
+        if(root.left == None):
+            return root.right
+        if(root.right == None):
+            return root.left
+        else:
+            succ = get_successor(root)
+            root.data = succ.data
+            root.right = delete(root.right, succ.data)
+    return root
+
+
+
+def InOrder(root):
+    if root is not None:
+        InOrder(root.left)
+        print(root.data, end=" ")
+        InOrder(root.right)
+
+
+root = insert(None, 20)
+root = insert(root, 15)
+root = insert(root, 30)
+root = insert(root, 40)
+root = insert(root, 12)
+root = insert(root, 18)
+root = insert(root, 25)
+root = insert(root, 50)
+
+InOrder(root)
+
+delete(root, 30)
+print()
+InOrder(root)
